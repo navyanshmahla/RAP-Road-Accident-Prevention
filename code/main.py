41787e91bbe011ce4521c7721bd6968df721a208
Navyansh Mahla <navyanshmahla@Navyanshs-MacBook-Air.local>
2022-07-16 23:29:17 +0530

    pushed trained model, main.py

---
+++ code/main.py
@@ -0,0 +1,106 @@
+import cv2
+import os
+from keras.models import load_model
+import numpy as np
+from pygame import mixer
+import time
+
+
+mixer.init()
+sound = mixer.Sound('crash.wav')
+
+face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
+leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
+reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
+
+
+
+label=['Close','Open']
+
+model = load_model('models/CNNmodel.h5')
+path = os.getcwd()
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+count=0
+score=0
+th=2
+right_pred=[99]
+left_pred=[99]
+
+while(True):
+    ret, frame = cap.read()
+    height,width = frame.shape[:2] 
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    faces = face.detectMultiScale(gray,minAdjacent=5,scaleFactor=1.1,minSize=(25,25))
+    left_eye = leye.detectMultiScale(gray)
+    right_eye =  reye.detectMultiScale(gray)
+
+    cv2.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y) , (x+w,y+h) , (100,100,100) , 1 )
+
+    for (x,y,w,h) in right_eye:
+        r_eye=frame[y:y+h,x:x+w]
+        count=count+1
+        r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
+        r_eye = cv2.resize(r_eye,(24,24))
+        r_eye= r_eye/255
+        r_eye=  r_eye.reshape(24,24,-1)
+        r_eye = np.expand_dims(r_eye,axis=0)
+        right_pred = model.predict(r_eye)
+        
+        print(right_pred[0])        
+        if(right_pred[0][0]>=0.85):
+            label='Closed' 
+        if(right_pred[0][0]<=0.20):
+            label='Open'
+        break
+
+    for (x,y,w,h) in left_eye:
+        l_eye=frame[y:y+h,x:x+w]
+        count=count+1
+        l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
+        l_eye = cv2.resize(l_eye,(24,24))
+        l_eye= l_eye/255
+        l_eye=l_eye.reshape(24,24,-1)
+        l_eye = np.expand_dims(l_eye,axis=0)
+        left_pred = model.predict(l_eye)
+        if(left_pred[0][0]>=0.85):
+            label='Closed'   
+        if(left_pred[0][0]<=0.20):
+            label='Opend'
+        break
+
+    if(right_pred[0][0]>=0.80 and left_pred[0][0]>=0.80):
+        score=score+1
+        cv2.putText(frame,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    else:
+        score=score-1
+        cv2.putText(frame,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    
+        
+    if(score<0):
+        score=0   
+    cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    if(score>5):
+        cv2.imwrite(os.path.join(path,'image.jpg'),frame)
+        try:
+            sound.play()
+            
+        except:  
+            pass
+        if(th<16):
+            th= th+2
+        else:
+            th=th-2
+            if(th<2):
+                th=2
+        cv2.rectangle(frame,(0,0),(width,height),(0,0,255),th) 
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
